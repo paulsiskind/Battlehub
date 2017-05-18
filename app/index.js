3ad7f3d422e783a35a27eb6df3e22228295564e1
paulsiskind <paulsiskind@gmail.com>
2017-05-18 16:41:09 -0600

    Update: A few basic components

--- app/index.js
+++ app/index.js
@@ -10,7 +10,50 @@ class App extends React.Component {
   }
 }
 
+class Users extends React.Component {
+  render() {
+    return (
+      <div>
+        <App />
+        <div>
+          <h1>Friends</h1>
+          <ul>
+            {this.props.list.filter(function(name){
+            return name.friend === true;
+           
+          }).map(function(names){
+             return <li key={names.name}>{names.name}</li>;
+            })}
+          </ul>
+          
+          <hr />
+          
+          <h1> Non Friends </h1>
+          <ul>
+            {this.props.list.filter(function(name){
+            return name.friend !== true;
+           
+          }).map(function(names){
+             return <li key={names.name}>{names.name}</li>;
+            })}
+          </ul>        
+        </div>
+      </div>
+    )
+  }
+}
+
+
 ReactDOM.render(
-  <App />,
+  
+    <Users list={[
+      { name: 'Tyler', friend: true },
+      { name: 'Ryan', friend: true },
+      { name: 'Michael', friend: false },
+      { name: 'Mikenzi', friend: false },
+      { name: 'Jessica', friend: true },
+      { name: 'Dan', friend: false } ]} 
+    />,
+
   document.getElementById('app')
 );
