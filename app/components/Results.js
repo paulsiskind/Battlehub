65508a4421e974cce896867220dd4b8837142dee
paulsiskind <paulsiskind@gmail.com>
2017-05-22 14:24:07 -0600

    Update: Added Results Route

---
+++ app/components/Results.js
@@ -0,0 +1,14 @@
+module.exports= Results;
+
+var React = require('react');
+
+class Results extends React.Component {
+  render() {
+  	console.log(this.props)
+    return (
+      <div>Results!</div>
+    )
+  }
+}
+
+module.exports = Results;
