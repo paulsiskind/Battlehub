684f9c26ddf2ba582adfe9a6ed07cbab492fb125
paulsiskind <paulsiskind@gmail.com>
2017-05-23 13:01:19 -0600

    placeholder

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,12 +1,109 @@
 module.exports= Results;
 
 var React = require('react');
+var queryString = require('query-string');
+var PropTypes = require('prop-types')
+var api = require('../utils/api');
+var Link = require('react-router-dom');
+
+
+function Player(props){
+	console.log(props)
+	return(
+		<div>
+			<h1 className='header'>{props.label}</h1>
+			 <img
+			    
+          className='avatar'
+          src={props.profile.avatar_url}
+          alt={'Avatar for ' + props.profile.login}
+        />
+      <h2 className="column">{props.profile.login}</h2>
+ 			<h2 className="column"> Score: {props.score}</h2>
+
+		</div>
+		)
+}
+
+Player.proptypes = {
+	label: PropTypes.string.isRequired,
+	score: PropTypes.number.isRequired,
+	profile: PropTypes.object.isRequired
+}
 
 class Results extends React.Component {
+	constructor(props){
+		super(props);
+
+		this.state = {
+			winner: null,
+			loser: null,
+			error: null,
+			loading: true
+		}
+	}
+
+	componentDidMount(){
+  	var players = queryString.parse(this.props.location.search)
+  	
+    api.battle([
+      players.playerOneName,
+      players.playerTwoName
+    ]).then(function (players) {
+      if (players === null) {
+        return this.setState(function () {
+          return {
+            error: 'Looks like there was an error. Check that both users exist on Github.',
+            loading: false,
+          }
+        });
+      }
+
+      this.setState(function () {
+        return {
+          error: null,
+          winner: players[0],
+          loser: players[1],
+          loading: false,
+        }
+      });
+    }.bind(this));
+  }
   render() {
-  	console.log(this.props)
+  	var error = this.state.errror;
+  	var winner = this.state.winner;
+  	var loser = this.state.loser;
+  	var loading = this.state.loading;
+  	
+
+  	if(loading===true){
+  		return <p>Loading</p>
+  	}
+
+  	if(error){
+  		return (
+  			<div>
+  			<p>{error}</p>
+  			<Link to='/battle'>Reset</Link>
+  			</div>
+  			)
+  	}
     return (
-      <div>Results!</div>
+     // <div>{JSON.stringify(this.state, null, 2)}</div>
+    	<div className="row">
+    		<Player
+    			label='Winner'
+    			score={winner.score}
+    			profile={winner.profile}
+    		/>
+
+    		<Player
+    			label='Loser'
+    			score={loser.score}
+    			profile={loser.profile}
+    		/>
+
+    	</div>
     )
   }
 }
